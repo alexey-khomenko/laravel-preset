8dde9e6995fbda63e0bd5a51542894c01e6bfa02
alexey-khomenko <alexey.alexeevich.khomenko@gmail.com>
2020-11-26 15:46:42 +0200

    tailwind components apply

--- Makefile
+++ Makefile
@@ -68,4 +68,3 @@ memory:
 perm:
 	sudo chgrp -R www-data storage bootstrap/cache
 	sudo chmod -R ug+rwx storage bootstrap/cache
-#   sudo chown
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,13 +16,10 @@
     </div>
 
     <div class="w-40 mx-auto">
-    <picture class="block h-0 w-full relative" style="padding-bottom: 100%">
-        <source srcset="{{ emptySrc() }}" type="image/webp"
-                data-srcset="{{ mixAsset('images/logo.webp') }}" />
-        <img src="{{ emptySrc() }}" class="lazyload h-full w-full block absolute left-0 top-0" alt="Лого"
-             data-src="{{ mixAsset('images/logo.png') }}" />
-    </picture>
+        <picture class="image-ratio-picture" style="padding-bottom: 100%">
+            <source srcset="{{ emptySrc() }}" type="image/webp" data-srcset="{{ mixAsset('images/logo.webp') }}"/>
+            <img src="{{ emptySrc() }}" class="image-ratio-img lazyload" alt="Лого"
+                 data-src="{{ mixAsset('images/logo.png') }}"/>
+        </picture>
     </div>
-
-    <hr class="border-black"/>
 @endsection
